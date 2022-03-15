ae1f4d72f5d9dd7c6b761a9b284b2d54ee08b3ad
Alex Belnitskii <belnitskii@mail.ru>
2022-03-15 12:57:55 +0300

    HW0

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
@@ -5,26 +8,56 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        Arrays.fill(storage, null);
     }
 
     void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        return Arrays.stream(storage)
+                .filter(Objects::nonNull)
+                .filter(resume -> resume.toString().equals(uuid)).findFirst().orElse(null);
     }
 
     void delete(String uuid) {
+        int indexOfDelete = -1;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                break;
+            }
+            if (storage[i].toString().equals(uuid)) {
+                indexOfDelete = i;
+            }
+        }
+        if (indexOfDelete >= 0) {
+            for (int i = indexOfDelete; i < storage.length - 1; i++) {
+                if (storage[i] == null) {
+                    break;
+                }
+                Resume toBeDeleted = storage[i];
+                storage[i] = storage[i + 1];
+                storage[i + 1] = toBeDeleted;
+
+            }
+        }
+        storage[storage.length - 1] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.stream(storage).filter(Objects::nonNull).toArray(Resume[]::new);
     }
 
     int size() {
-        return 0;
+        return (int) Arrays.stream(storage).filter(Objects::nonNull).count();
     }
 }
